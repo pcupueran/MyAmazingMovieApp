3f9e378410b692eab9ed6e2f2ea0823e8fe7d44d
Patricia <pa1809ty@gmail.com>
2015-10-22 15:27:37 +0100

    Modifyprotect_from_ csrf to false, to enable updating field in the database

--- config/apps.rb
+++ config/apps.rb
@@ -29,7 +29,7 @@ Padrino.configure_apps do
   # enable :sessions
   set :session_secret, '7339b197d4aad39ff96435e8c80e1c0102456c1a05ee383f273726e7501d6820'
   set :protection, :except => :path_traversal
-  set :protect_from_csrf, true
+  set :protect_from_csrf, false
 end
 
 # Mounts the core application for this project
