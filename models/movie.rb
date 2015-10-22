a908a11856a3494f13240f17af54160967bc7cc2
Patricia <pa1809ty@gmail.com>
2015-10-22 15:33:28 +0100

    JSON file added to use the stubbing technique to get persistent data while testing

---
+++ models/movie.rb
@@ -0,0 +1,19 @@
+class Movie < ActiveRecord::Base
+  before_save :set_slug
+
+  def self.get_film_info(title)
+    imdb_data = HTTParty.get("http://www.omdbapi.com/?t=#{title}")
+    m = Movie.new(title: imdb_data["Title"])
+    m.year = imdb_data["Year"]
+    m.description = imdb_data["Description"]
+    m.poster = imdb_data["Poster"]
+    m.runtime = imdb_data["Runtime"]
+    m.save!
+    m
+  end
+
+  protected
+  def set_slug
+    self.slug = title.chomp.strip.downcase.gsub(" ", "-")
+  end
+end
